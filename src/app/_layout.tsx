cd90acf9281b0e1268f2d3a8c8997a98c17e6db5
Noah Geerts <ngeerts@student.ubc.ca>
2026-08-18 15:37:52 -0700

    Add basic functional login/logout flow

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,19 +1,51 @@
 import { Stack } from "expo-router";
-import { useAuth0, Auth0Provider } from "react-native-auth0";
+import { useEffect } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { Auth0Provider, useAuth0 } from "react-native-auth0";
 
 export default function Layout() {
-  const {user} = useAuth0();
   return (
     <Auth0Provider
       domain="dev-7depnj7pxm3mr8iz.us.auth0.com"
       clientId="7UFjBQjLFsI5SrwF29TYb4beI9s1YOyL"
     >
-      <Stack>
-        <Stack.Protected guard={!!user}>
-          <Stack.Screen name="(authenticated)" />
-        </Stack.Protected>
-        <Stack.Screen name="index" />
-      </Stack>
+      <AuthGuard />
     </Auth0Provider>
   );
 }
+
+function AuthGuard() {
+  const { authorize, isLoading, hasValidCredentials, user } = useAuth0();
+
+  // Force login if user becomes logged out
+  useEffect(() => {
+    async function checkIfAuthenticated() {
+      const isAuthenticated = await hasValidCredentials();
+      if (!isAuthenticated) {
+        authorize();
+      }
+    }
+
+    checkIfAuthenticated();
+  }, [user]);
+
+  if (!user) {
+    return (
+      <View style={styles.container}>
+        <View style={{ height: 200 }} />
+        <Text>Welcome to Progress</Text>
+        <Button title="Log in" onPress={() => authorize()} />
+      </View>
+    );
+  }
+
+  return <Stack />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+});
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,35 +1,13 @@
-import { Redirect } from "expo-router";
-import { useState } from "react";
-import { Text, View, StyleSheet, TextInput, Button } from "react-native";
+import { Text, View, StyleSheet, Button } from "react-native";
 import { useAuth0 } from "react-native-auth0";
 
-export default function Dashboard() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const { authorize, user } = useAuth0();
-
-  if(!!user) {
-    return <Redirect href="/(authenticated)/index"/>
-  }
+export default function Index() {
+  const { clearSession } = useAuth0();
 
   return (
     <View style={styles.container}>
-      <Text>Logged out</Text>
-      <View style={{ flexDirection: "column", gap: 10 }}>
-        <TextInput
-          style={{ fontSize: 24 }}
-          placeholder="email"
-          value={email}
-          onChangeText={(t) => setEmail(t)}
-        />
-        <TextInput
-          style={{ fontSize: 24 }}
-          placeholder="password"
-          value={password}
-          onChangeText={(t) => setPassword(t)}
-        />
-        <Button title="Log in" onPress={() => authorize()} />
-      </View>
+      <Text>Logged In</Text>
+      <Button title="Log Out" onPress={() => clearSession()} />
     </View>
   );
 }
@@ -38,6 +16,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
   },
 });
